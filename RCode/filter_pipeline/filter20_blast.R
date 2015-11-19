2d8ec01a4498dc83b8d2c44035a9e597bdd4597c
Pavel Senin <seninp@gmail.com>
2015-11-19 14:25:35 +0100

    fixing filters

--- RCode/filter_pipeline/filter20_blast.R
+++ RCode/filter_pipeline/filter20_blast.R
@@ -14,18 +14,21 @@ keepers_list <- args[3]
 llist <- read.table(contigs_llist, header = FALSE, sep = " ", quote = "",  stringsAsFactors = FALSE,
                     comment.char = "", colClasses = c("character","integer"))
 tsv <- fread(best_tsv)
-setnames(tsv, c("query","hit","desc","identity","bits","qlen","alen","gaps","evalue"))
-tsv$qCoverage <- tsv$alen/tsv$qlen * 100
+setnames(tsv, c("qName","tName","desc","identity","bits","qlen","alen","gaps","evalue"))
+tsv$"%qCoverage" <- tsv$alen/tsv$qlen * 100
 #
 # [0.3] filter the input by coverage
 #
 cov_keepers <- tsv[tsv$"%qCoverage" < 20.0,]$qName
+summary(tsv[tsv$qName %in% cov_keepers, ]$"%qCoverage" )
 #
 # [0.4] filter the input by no hits
 #
-no_hits_set <- setdiff(contigs_llist$V1, tsv$qName)
+no_hits_set <- setdiff(llist$V1, tsv$qName)
 #
 # [0.5] compose the set and save
 #
+print(paste("keepers homology:",length(cov_keepers),"keepers no hits:",length(no_hits_set)))
 keepers <- unique( c(no_hits_set, cov_keepers) )
-write.table(keepers,keepers_list,row.names = F,col.names = F,quote = F)
+#
+write.table(keepers,keepers_fname,row.names = F,col.names = F,quote = F)
--- RCode/filter_pipeline/filter20_blat.R
+++ RCode/filter_pipeline/filter20_blat.R
@@ -5,25 +5,28 @@ args <- commandArgs(trailingOnly = TRUE)
 print(paste("arguments supplied:",args))
 #
 # [0.1] take care about the input file names
-contigs_llist <- args[1]
-best_tsv <- args[2]
-keepers_list <- args[3]
+llist_fname <- args[1]
+tsv_fname <- args[2]
+keepers_fname <- args[3]
 #
 # [0.2] read the input
 #
-llist <- read.table(contigs_llist, header = FALSE, sep = " ", quote = "",  stringsAsFactors = FALSE,
+llist <- read.table(llist_fname, header = FALSE, sep = " ", quote = "",  stringsAsFactors = FALSE,
                     comment.char = "", colClasses = c("character","integer"))
-tsv <- fread(best_tsv)
+tsv <- fread(tsv_fname)
 #
 # [0.3] filter the input by coverage
 #
 cov_keepers <- tsv[tsv$"%qCoverage" < 20.0,]$qName
+summary(tsv[tsv$qName %in% cov_keepers, ]$"%qCoverage" )
 #
 # [0.4] filter the input by no hits
 #
-no_hits_set <- setdiff(contigs_llist$V1, tsv$qName)
+no_hits_set <- setdiff(llist$V1, tsv$qName)
 #
 # [0.5] compose the set and save
 #
+print(paste("keepers homology:",length(cov_keepers),"keepers no hits:",length(no_hits_set)))
 keepers <- unique( c(no_hits_set, cov_keepers) )
-write.table(keepers,keepers_list,row.names = F,col.names = F,quote = F)
+#
+write.table(keepers,keepers_fname,row.names = F,col.names = F,quote = F)
